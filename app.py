b58dbc3e38a9566bd6170f274d7e13c4bda52f87
joseph-kinyuru <josephkinyuru22@gmail.com>
2023-09-27 17:37:03 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+
+from flask import Flask, jsonify, make_response
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+
+from models import db, Bird
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://joseph:ssizEoLOcv0Ce1car3URSDzwwk3AudAZ@dpg-cka26lvs0fgc7392pm2g-a.oregon-postgres.render.com/bird_app_v3fv'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+api = Api(app)
+
+class Birds(Resource):
+
+    def get(self):
+        birds = [bird.to_dict() for bird in Bird.query.all()]
+        return make_response(jsonify(birds), 200)
+
+api.add_resource(Birds, '/birds')
